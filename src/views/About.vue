082c10ef73a7c2f506398e9d9bf931b556837638
Timo Holvoet <timo.holvoet@student.howest.be>
2020-01-13 09:41:34 +0100

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Testpagina</h1>
+    <p>lokaal ingeladen usdz file:</p>
+
+    <a rel="ar" href="assets/heart.usdz">
+      <img src="../assets/logo.png" alt />
+    </a>
+
+    <p>lokaal ingeladen usdz file:</p>
+    <a href="../assets/heart.usdz">iMessage Heart Tap-back</a>
+    <p>file hosted op github:</p>
+    <a
+      href="https://github.com/TimoHolvoet/ProjectIV/blob/master/ar_js/vue_test/src/assets/usdz/toy_drummer.usdz"
+    >Toydrummer!</a>
+    
+    <p>extern gehoste usdz file: (werkend)</p>
+    <a href="https://webkit.org/demos/ar/heart.usdz">iMessage Heart Tap-back</a>
+    <!-- <model-viewer
+      src="@/assets/usdz/toy_drummer.usdz"
+      alt="A 3D model of a toy drummer"
+      auto-rotate
+      camera-controls
+      background-color="#455A64"
+    ></model-viewer>-->
   </div>
 </template>
